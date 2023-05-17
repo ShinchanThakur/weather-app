f2c5a84a37bd82ba0d66b4efc2635617f4e6537b
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-17 17:14:51 +0530

    Converted geocode request to raw-https-request

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,25 +1,37 @@
-const request = require('postman-request');
+const https = require('https');
 const config = require('../config.json');
 
 const geocode = (address, callback) => {
     const geocodeApiKey = config.geocode_api_key;
     const url = 'https://www.mapquestapi.com/geocoding/v1/address?key=' + geocodeApiKey + '&location=' + encodeURIComponent(address);
 
-    request({ url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined);
-        }
-        else if (response.body.results[0].locations[0].adminArea5 === '')
-            callback('Unable to find location. Try another search.', undefined);
-        else {
-            const lattitude = response.body.results[0].locations[0].latLng.lat;
-            const longitude = response.body.results[0].locations[0].latLng.lng;
-            callback(undefined, {
-                lattitude,
-                longitude
-            });
-        }
-    })
+    const request = https.request(url, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        });
+
+        response.on('end', () => {
+            const body = JSON.parse(data);
+            if (body.results[0].locations[0].adminArea5 === '')
+                callback('Unable to find location. Try another search.', undefined);
+            else {
+                const lattitude = body.results[0].locations[0].latLng.lat;
+                const longitude = body.results[0].locations[0].latLng.lng;
+                callback(undefined, {
+                    lattitude,
+                    longitude
+                });
+            }
+        })
+    });
+
+    request.on('error', (error) => {
+        callback('Unable to connect to location services!', undefined);
+    });
+
+    request.end();
 }
 
 module.exports = geocode;
