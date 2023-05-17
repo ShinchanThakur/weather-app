80e307f06acc056561d8a8bb5bc9e2a205b7ea60
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-17 17:17:28 +0530

    Converted forecast request to raw-https-request

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,24 +1,36 @@
-const request = require('postman-request');
+const http = require('http');
 const config = require('../config.json');
 
 const forecast = (lattitude, longitude, callback) => {
     const forcastApiKey = config.forecast_api_key;
     const url = 'http://api.weatherstack.com/current?access_key=' + forcastApiKey + '&query=' + lattitude + ',' + longitude;
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather services!', undefined);
-        }
-        else if (body.error)
-            callback('Unable to find location. Try another search.', undefined);
-        else {
-            //use weather_icons later
-            callback(undefined, {
-                location: body.location.name + ', ' + body.location.country,
-                forecast: body.current.weather_descriptions[0] + ': It is currently ' + body.current.temperature + ' degress out. There is ' + body.current.cloudcover + '% cloud cover.'
-            });
-        }
-    })
+    const request = http.request(url, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        });
+
+        response.on('end', () => {
+            const body = JSON.parse(data);
+            if (body.error)
+                callback('Unable to find location. Try another search.', undefined);
+            else {
+                //use weather_icons later
+                callback(undefined, {
+                    location: body.location.name + ', ' + body.location.country,
+                    forecast: body.current.weather_descriptions[0] + ': It is currently ' + body.current.temperature + ' degress out. There is ' + body.current.cloudcover + '% cloud cover.'
+                });
+            }
+        })
+    });
+
+    request.on('error', (error) => {
+        callback('Unable to connect to weather services!', undefined);
+    });
+
+    request.end();
 }
 
 module.exports = forecast;
