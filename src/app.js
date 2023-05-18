cdab55b304eb1cf71273226b0807c9d2ffc043be
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-18 16:58:38 +0530

    Added API for weather forecast

--- src/app.js
+++ src/app.js
@@ -1,26 +1,33 @@
-const { argv } = require('process');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 const express = require('express');
 
 const app = express();
 
-// const address = argv[2];
-
-// geocode(address, (error, { lattitude, longitude } = {}) => {
-//     if (error)
-//         return console.log(error);
-//     forecast(lattitude, longitude, (error, response) => {
-//         if (error)
-//             return console.log(error);
-//         console.log(response.location);
-//         console.log(response.forecast);
-//     });
-// });
-
-app.get('', (req, res) => {
-    res.send({
-        message: 'This is home page, it is just for testing purpose'
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        });
+    }
+    geocode(req.query.address, (error, { lattitude, longitude } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            });
+        }
+        forecast(lattitude, longitude, (error, response) => {
+            if (error) {
+                return res.send({
+                    error
+                });
+            }
+            res.send({
+                location: response.location,
+                forecast: response.forecast,
+                yourQuery: req.query.address
+            })
+        });
     });
 });
 
